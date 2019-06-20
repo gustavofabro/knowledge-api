5a5b25716bbe22a4fd463a904740992beb28fd42
Gustavo <gustavofabro.f@gmail.com>
2019-06-20 20:07:15 -0300

    block admin register

--- api/user.js
+++ api/user.js
@@ -2,35 +2,40 @@ const bcrypt = require('bcryptjs')
 
 module.exports = app => {
     const { existsOrError, notExistsOrError, equalsOrError } = app.api.validation
- 
+
     const encryptPassword = password => {
         const salt = bcrypt.genSaltSync(10)
 
         return bcrypt.hashSync(password, salt)
     }
- 
+
     const save = async (req, res) => {
         const user = { ...req.body }
 
         if (req.params.id) {
             user.id = req.params.id
         }
-        
+
+        if (!req.originalUrl.startsWith('/users')
+            || (!req.user || !req.user.admin)) {
+            user.admin = false
+        }
+
         try {
             existsOrError(user.name, 'Nome não informado')
             existsOrError(user.email, 'Email não informado')
             existsOrError(user.password, 'Senha não informado')
             existsOrError(user.confirmPassword, 'Confirmação de senha inválida')
-            equalsOrError(user.password, user.confirmPassword, 
+            equalsOrError(user.password, user.confirmPassword,
                 'Senhas não conferem')
 
             const userFromDb = await app.db('users')
                 .where({ email: user.email }).first()
-            
+
             if (!user.id) {
                 notExistsOrError(userFromDb, 'Usuário já cadastrado')
             }
-        } catch(msg) {
+        } catch (msg) {
             return res.status(400).send(msg)
         }
 
@@ -45,7 +50,7 @@ module.exports = app => {
                 .whereNull('deletedAt')
                 .then(_ => res.status(204).send())
                 .catch(err => res.status(500).send(err))
-            
+
         } else {
             app.db('users')
                 .insert(user)
@@ -66,7 +71,7 @@ module.exports = app => {
     const getById = (req, res) => {
         app.db('users')
             .select('id', 'name', 'email', 'admin')
-            .where({ id: req.params.id})
+            .where({ id: req.params.id })
             .whereNull('deletedAt')
             .then(user => res.json(user))
             .catch(err => res.status(500).send(err))
@@ -85,8 +90,8 @@ module.exports = app => {
 
             existsOrError(rowsUpdated, 'Usuário não foi encontrado.')
 
-            res.status(204).send()   
-        } catch(msg) {
+            res.status(204).send()
+        } catch (msg) {
             res.status(400).send(msg)
         }
     }
--- config/routes.js
+++ config/routes.js
@@ -1,8 +1,11 @@
 const admin = require('./admin')
 
 module.exports = app => {
-    app.post('/signup', app.api.user.save)
-    app.post('/signin', app.api.auth.signin)
+    app.route('/signup')
+        .all(app.config.passport.authenticate())
+        .post(app.api.user.save)
+
+    app.route('/signin', app.api.auth.signin)
     app.post('/validateToken', app.api.auth.validateToken)
 
     app.route('/users')
