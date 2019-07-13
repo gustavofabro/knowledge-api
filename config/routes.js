4378f589252c256a601c5cb49dc362ca3ee87e75
Gustavo <gustavofabro.f@gmail.com>
2019-07-13 15:57:48 -0300

    fix route

--- config/routes.js
+++ config/routes.js
@@ -5,7 +5,7 @@ module.exports = app => {
         .all(app.config.passport.authenticate())
         .post(app.api.user.save)
 
-    app.route('/signin', app.api.auth.signin)
+    app.post('/signin', app.api.auth.signin)
     app.post('/validateToken', app.api.auth.validateToken)
 
     app.route('/users')
